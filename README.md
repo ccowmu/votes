14f125a9b7037481a557c9429574107ce3f87b4d
scott-linder <scott.b.linder@wmich.edu>
2016-03-25 15:54:09 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # votes
+
 Voting app for club elections
+
+# setup
+
+Create a directory named `positions/`. In this directory, there should be a text
+file per position, with a candidate per line.
+
+For example if there are president and a vp positions, and foo and bar are
+running for president, and baz and qux are running for vp, the setup looks
+like:
+
+```
+$ mkdir positions
+$ echo foo >>positions/president
+$ echo bar >>positions/president
+$ echo baz >>positions/vp
+$ echo qux >>positions/vp
+```
+
+# output
+
+The output is under the `votes/` directory. There is one subdirectory per
+user, named by username. Under each user's directory there are files with
+identical names to those in `positions/`, but with only one line: the users
+vote for that position.
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>CCoWMU Votes</title>
+</head>
+<body>
+<?php
+function clean_username($s) {
+  return preg_replace("/[^a-zA-Z0-9]/", "", $s);
+}
+function not_dot($s) {
+  return !($s === "." or $s === "..");
+}
+if ($_SERVER['REQUEST_METHOD'] === "POST"):
+  $ldap = ldap_connect("localhost") or die("failed to connect to ldap");
+  ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3) or die("failed to set protocol");
+  $user = clean_username($_POST['username']);
+  $binddn = "uid=$user,cn=members,dc=yakko,dc=cs,dc=wmich,dc=edu";
+  $pass = $_POST['password'];
+  if ($user and $pass and $bind = ldap_bind($ldap, $binddn, $pass)) {
+    echo "<p>Thank you for your vote, $user!</p>";
+    echo "<dl>";
+    mkdir('votes/' . $user, 0777, true);
+    foreach (array_filter(scandir("positions"), not_dot) as $position) {
+      $vote = clean_username($_POST[$position]);
+      echo "<dt>$position</dt>";
+      echo "<dd>$vote</dd>";
+      file_put_contents('votes/' . $user . '/' . $position, $vote . "\n");
+    }
+    echo "</dl>";
+  } else {
+    echo "<p>Login failed; go back in your browser history and try again.</p>";
+  }
+else: ?>
+<h1>CCoWMU Votes</h1>
+<form method="POST">
+<?php foreach (array_filter(scandir("positions"), not_dot) as $position): ?>
+  <fieldset>
+    <legend><?php echo $position ?></legend>
+  <?php foreach (array_map(trim, file('positions/' . $position)) as $candidate): ?>
+    <div>
+    <input type="radio" name="<?php echo $position ?>" value="<?php echo $candidate ?>">
+      <label for="<?php echo $candidate ?>"><?php echo $candidate ?></label>
+    </div>
+  <?php endforeach; ?>
+  </fieldset>
+<?php endforeach; ?>
+<div>
+  <label for="username">username</label>
+  <input type="text" name="username">
+</div>
+<div>
+  <label for="password">password</label>
+  <input type="password" name="password">
+</div>
+<input type="submit" value="cast vote">
+</form>
+<?php endif; ?>
+</body>
+</html>
